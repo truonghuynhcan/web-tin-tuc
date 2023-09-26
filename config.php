ef13975c1530e89fccf8553cceb0eb4702b122fa
truonghuynhcan@gmail.com <truonghynhcan@gmail.com>
2023-09-26 22:15:18 +0700

    fix config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  const DB_HOST = 'https://truonghuynhcan.id.vn';
+  const DB_USERNAME = 'fifqrvy41ga2_webtintuc';
+  const DB_PASSWORD = "canWPPR2004@";
+  const DB_NAME = "fifqrvy41ga2_webtintuc";
+  const BASE_DIR = "";
+  const PAGE_SIZE = 10;
+  const NUM_RELATED_POST = 3;
+  const NUM_POST_HOMEPAGE = 20;
+?>
