e9e681ed1ccd0f91a3ef280eb841668baeba6bd8
TrươngHuỳnhCan <144352376+truonghuynhcan@users.noreply.github.com>
2023-09-26 22:49:58 +0700

    Update config.php
    delete dir

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
   const DB_USERNAME = 'fifqrvy41ga2_webtintuc';
   const DB_PASSWORD = "canWPPR2004@";
   const DB_NAME = "fifqrvy41ga2_webtintuc";
-  const BASE_DIR = "truonghuynhcan2.shop/index";
+  const BASE_DIR = "";
   const PAGE_SIZE = 10;
   const NUM_RELATED_POST = 3;
   const NUM_POST_HOMEPAGE = 20;
