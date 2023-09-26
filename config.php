42f6431163bd0b26445e30a1a28c1de84d5f79a7
TrươngHuỳnhCan <144352376+truonghuynhcan@users.noreply.github.com>
2023-09-26 22:36:26 +0700

    Update config.php
    fix db_host and base_dir

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-  const DB_HOST = 'truonghuynhcan.id.vn';
+  const DB_HOST = 'truonghuynhcan2.shop';
   const DB_USERNAME = 'fifqrvy41ga2_webtintuc';
   const DB_PASSWORD = "canWPPR2004@";
   const DB_NAME = "fifqrvy41ga2_webtintuc";
-  const BASE_DIR = "index";
+  const BASE_DIR = "truonghuynhcan2.shop/index";
   const PAGE_SIZE = 10;
   const NUM_RELATED_POST = 3;
   const NUM_POST_HOMEPAGE = 20;
