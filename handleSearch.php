e1361d15d8c1d09bbea40eaf90986a3d63da2579
truonghuynhcan@gmail.com <truonghynhcan@gmail.com>
2023-09-26 23:09:20 +0700

    xu ly xung dot

--- handleSearch.php
+++ handleSearch.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
    require_once './functions.php';
    $value=$_POST['value']??"tôi là một người mới mong bạn có thể giuo";
@@ -17,24 +16,4 @@
       ];
   }
   echo json_encode($res)
-=======
-<?php
-   require_once './functions.php';
-   $value=$_POST['value']??"tôi là một người mới mong bạn có thể giuo";
-   
-  $sql="SELECT * FROM tin WHERE TieuDe like '%".$value."%'  limit 10";
-  $result=executeResult($sql);
-  if($result!=null){
-    $res=[
-      'status'=>1,
-      'result'=>$result
-    ];
-  }
-  else{
-    $res=[
-        'status'=>0
-      ];
-  }
-  echo json_encode($res)
->>>>>>> 42f6431163bd0b26445e30a1a28c1de84d5f79a7
 ?>
